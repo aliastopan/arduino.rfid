7081861e544d189fad49bb54fea2f9286c61273b
einharan <taufan.augusta@protonmail.com>
2021-12-09 12:23:04 +0700

    build: add MFRC522

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+void setup() 
+{
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
 }
